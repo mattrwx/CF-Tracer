d2dade2ec5393fcfacf72e454c75f3ef8f7e6f46
matt <matthewwyattdominguez@gmail.com>
2026-04-13 03:07:48 -0400

    Update dumper.hpp

--- dumper.hpp
+++ dumper.hpp
@@ -1,7 +1,10 @@
 #include <Windows.h>
 #include <fstream>
 
+
+
+
 namespace dumper
 {
-	void dump(std::string instruction, void* next_address, bool jmp_taken, const _EXCEPTION_POINTERS* exception_info);
+	void dump(std::string instruction, const _EXCEPTION_POINTERS* exception_info);
 }
